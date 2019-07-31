db140864d24393d7feb3415937dc7a446cf01485
Rachel Kim <rayykim323@gmail.com>
2019-07-31 12:44:05 -0700

    added styling

--- src/client/components/Recycle.tsx
+++ src/client/components/Recycle.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Recycle = (props: any) => {
   console.log('material info', props.materialInfo);
@@ -8,24 +9,49 @@ const Recycle = (props: any) => {
   const totalCount = materialInfo.lessThan + materialInfo.greaterThan;
 
   return (
-    <div>
+    <MaterialStyled>
       <div>{props.material}</div>
       <div>Total Amount: {totalPrice}</div>
-      <div>
-        <img src="" />
-        Less than 24oz
-        <button id="">-</button>#{}
-        <button id="">+</button>
-      </div>
-      <div>
-        <img src="" />
-        Greater than 24oz
-        <button id="">-</button>#{}
-        <button id="">+</button>
-      </div>
-      <div>Total Item Count: {totalCount} </div>
-    </div>
+      <AmountStyled>
+        <div>
+          <img src="" />
+          Less than 24oz
+        </div>
+        <div>
+          <button id="">-</button>
+          {materialInfo.lessThan}
+          <button id="">+</button>
+        </div>
+      </AmountStyled>
+      <AmountStyled>
+        <div>
+          <img src="" />
+          Greater than 24oz
+        </div>
+        <div>
+          <button id="">-</button>
+          {materialInfo.greaterThan}
+          <button id="">+</button>
+        </div>
+      </AmountStyled>
+      <TotalItemStyled>Total Item Count: {totalCount} </TotalItemStyled>
+    </MaterialStyled>
   );
 };
 
+
+const MaterialStyled = styled.div `
+    display:flex;
+    flex-direction: column;
+    padding: 20px;
+`
+const AmountStyled = styled.div  `
+  display: flex;
+  border: 1px solid blue;
+  justify-content:space-between
+`
+const TotalItemStyled = styled.div`
+  text-align: right;
+  
+`
 export default Recycle;
--- src/client/containers/MainContainer.tsx
+++ src/client/containers/MainContainer.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Recycle from '../components/Recycle';
+import styled from 'styled-components'
 
 type material = string;
 const MainContainer = (props: any) => {
   return (
-    <div>
+    <MainContainerStyled>
       Recycling Categories:
       <Recycle materialInfo={props.type.glass} material="Glass" />
       <Recycle materialInfo={props.type.metal} material="Plastic" />
       <Recycle materialInfo={props.type.plastic} material="Metal" />
-    </div>
+    </MainContainerStyled>
   );
 };
 
 export default MainContainer;
+
+const MainContainerStyled = styled.div`
+  border: 2px solid red
+`
