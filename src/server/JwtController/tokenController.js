fef8cb5212064fb80c4bb9906993431949752f1b
Brian Kwok <briankwok@Brians-MacBook-Pro.local>
2019-07-31 14:59:16 -0700

    jwt controller

---
+++ src/server/JwtController/tokenController.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const { secret } = require("./config.js");
+
+module.exports = {
+  checkToken(req, res, next) {
+    let token = req.cookies.token;
+    if (!token || !token.startsWith("Bearer")) {
+      return next("invalid token format");
+    }
+
+    token = this.token.split("")[1];
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        return next(err);
+      }
+      req.token = decodedToken;
+      next();
+    });
+  },
+
+  signToken(req, res, next) {
+    let token = jwt.sign({ username: req.body.username }, secret);
+    res.cookie("token", `Bearer ${token}`);
+  }
+};
