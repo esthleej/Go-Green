87c0d2a81c8b0a22e6f14b0ddb027dd875215c14
Rachel Kim <rayykim323@gmail.com>
2019-07-30 20:17:31 -0700

    setting up contaier files

---
+++ src/client/containers/HistoryContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function HistoryContainer() {
+  return (
+<div>
+
+</div>
+  );
+}
+
+export default HistoryContainer;
---
+++ src/client/containers/SignedInContainer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+
+
+function SignedInContainer() {
+  return (
+  <div>
+
+  </div>
+  );
+}
+
+export default SignedInContainer;
