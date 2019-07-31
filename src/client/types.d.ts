e0c349ef5d3608e300516fd71f88ffe1c449fc5a
Esther Lee <37000206+esthleej@users.noreply.github.com>
2019-07-31 12:01:25 -0700

    add another interface

--- src/client/types.d.ts
+++ src/client/types.d.ts
@@ -3,12 +3,31 @@ export interface State {
   totalPaid: Number;
   totalItemsRecycled: Number;
   recyclingHistory: Recycling[];
+  glass: GreaterLess;
+  plastic: GreaterLess;
+  metal: GreaterLess;
+  type: Types;
 }
 
+export interface Types {
+  glass: GreaterLess;
+  plastic: GreaterLess;
+  metal: GreaterLess;
+}
 export interface Recycling {
   date: Date;
   amountPair: Number;
   amountRecycled: Number;
 }
 
+export interface GreaterLess {
+  lessThan: Number;
+  greaterThan: Number;
+}
+
 export type Material = string;
+
+export interface Type {
+  totalPrice: Number;
+  totalCount: Number;
+}
