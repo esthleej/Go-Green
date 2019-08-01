e294aa8b5773146672e9af53a29adbc5577d9232
Maximilian Gonzalez <thamaxlg@gmail.com>
2019-07-31 19:07:24 -0700

    login page now stores a JWT as a cookie on the broswer when user signs in or successfully logs in

--- src/client/components/Login.tsx
+++ src/client/components/Login.tsx
@@ -1,14 +1,72 @@
-import React from 'react'
+import React, {useRef} from 'react'
 
 const Login = () => {
+  const usernameLogin = useRef(null);
+  const passwordLogin = useRef(null);
+  const usernameSignup = useRef(null);
+  const passwordSignup = useRef(null);
+
+  const handleLogin = () => {
+    const username = usernameLogin.current.value;
+    const password = passwordLogin.current.value;
+    if (username !== '' && password !== '') {
+      fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username, password
+        })
+      })
+      .then(res => res.json())
+      .then(data => {
+        if (data === 'user verified') {
+          console.log('u logged in')
+        } else {
+          console.log('u DIDNT loggigng in')
+        }
+      })
+      .catch(err => {
+        console.log('u DIDNTS loggigng in')
+      });
+    }
+  };
+
+  const handleSignup = () => {
+    const password = passwordSignup.current.value;
+    const username = usernameSignup.current.value;
+    if (username !== '' && password !== '') {
+      fetch('/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username, password
+        })
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log('u signed up')
+      })
+      .catch(err => {
+        console.log('u didnt signed up')
+      });
+    }
+  };
+
   return (
     <div>
-      <h2>log in</h2>
-      <input type="text"></input>
-      <input type="text"></input>
-      <h2>sign up</h2>
-      <input type="text"></input>
-      <input type="text"></input>
+      <button onClick={handleLogin}>log in</button>
+      <br />
+      <input type="text" placeholder="username" ref={usernameLogin}></input>
+      <input type="text" placeholder="password" ref={passwordLogin}></input>
+      <br />
+      <button onClick={handleSignup}>sign up</button>
+      <br />
+      <input type="text" placeholder="username" ref={usernameSignup}></input>
+      <input type="text" placeholder="password" ref={passwordSignup}></input>
     </div>
   )
 };
